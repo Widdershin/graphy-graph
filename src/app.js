638c896f24acb706d36d69fb242498d0d3f2e24b
Nick Johnstone <ncwjohnstone@gmail.com>
2016-11-24 16:45:16 +1300

    Center graph rendering

--- src/app.js
+++ src/app.js
@@ -17,30 +17,53 @@ function App ({DOM}) {
       div('.derivatives', [
         input('.f', {attrs: {value: f}}),
 
-        h('svg', {attrs: {width, height, viewBox: `${-(width / 2)} ${(height / 2)} ${width} ${height}`}}, [
-          h('path', {attrs: {d: buildPath({width, height}, f), fill: 'none', stroke: 'white'}})
-        ]),
-
-        h('svg', {attrs: {width, height, viewBox: `${-(width / 2)} ${(height / 2)} ${width} ${height}`}}, [
-          h('path', {attrs: {d: buildDerivativePath({width, height}, f), fill: 'none', stroke: 'white'}})
-        ]),
-
-        h('svg', {attrs: {width, height, viewBox: `${-(width / 2)} ${(height / 2)} ${width} ${height}`}}, [
-          h('path', {attrs: {d: buildDerivativeDerivativePath({width, height}, f), fill: 'none', stroke: 'white'}})
-        ])
+        renderGraph({width, height, path: buildPath({width, height}, f)}),
+        renderGraph({width, height, path: buildDerivativePath({width, height}, f)}),
+        renderGraph({width, height, path: buildDerivativeDerivativePath({width, height}, f)}),
       ])
     )
   };
 }
 
+function renderGraph({width, height, path}) {
+  const left = -(width / 2);
+
+  return (
+    h('svg', {attrs: {width, height, viewBox: `${-(width / 2)} ${(height / 2)} ${width} ${height}`}}, [
+      h('line', {
+        attrs: {
+          stroke: '#333',
+          'stroke-width': 1,
+          x1: left,
+          y1: height,
+          x2: width / 2,
+          y2: height
+        }
+      }),
+
+      h('line', {
+        attrs: {
+          stroke: '#333',
+          'stroke-width': 1,
+          x1: 0,
+          y1: 0,
+          x2: 0,
+          y2: height * 2
+        }
+      }),
+      h('path', {attrs: {d: path, fill: 'none', stroke: 'white'}}),
+    ])
+  )
+}
+
 function pointsToPath ({width, height}, points) {
-  return `M 0 ${height} ` + points.map((point, index) => `L ${index} ${height - point}`).join(' ');
+  return `M ${-(width / 2)} ${height - points[0]} ` + points.slice(1).map((point, index) => `L ${index - width / 2} ${height - point}`).join(' ');
 }
 
 function buildPoints ({width, height}, fString) {
   const f = new Function('x', 'return ' + fString);
 
-  const points = new Array(width).fill(0).map((_, index) => f(index));
+  const points = new Array(width).fill(0).map((_, index) => f(index - width / 2));
 
   return points;
 }
